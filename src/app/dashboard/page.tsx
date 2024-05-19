b1a648fbd286a64c16f5c728f6dd70eb899c217b
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-05-19 16:17:48 +0300

    update

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export interface PageProps {}
+
+
+
+
+export default function Page({}: PageProps) {
+  return (
+    <main><h1 className="text-xl">Dashboard page</h1>
+    </main >
+  )
+}
+
+
