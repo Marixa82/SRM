3e1814b030adc0d2c5fd52b1ed22f57a2f75321c
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-05-20 15:35:10 +0300

    layout

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -14,7 +14,7 @@ export interface PageProps {}
 export default function Page({}: PageProps) {
   return (
     <>
-      <Header>Companied</Header>
+      <Header>Companies</Header>
       <Toolbar action={<AddCompanyButton />} ><SearchInput /></Toolbar>
       <CompanyTable>
         <CompanyRow id={1} category={"Products"} company={"Costco"} status={Status.Pending} promotion={true} country={"USA"} joinedDate={"02.19.2023"}  />
