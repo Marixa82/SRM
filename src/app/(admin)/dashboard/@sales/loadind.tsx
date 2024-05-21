b1b6ee2fe2e3f7603e4ffb263d28482d40240dc5
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-05-21 23:00:30 +0300

    add parallel routes

---
+++ src/app/(admin)/dashboard/@sales/loadind.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export interface LoadindProps {}
+
+export default function Loading({}: LoadindProps) {
+return (
+      <div>
+        Loading...
+      </div>
+    )
+}
+
+
+
--- src/app/(admin)/dashboard/newCompanies/page.tsx
+++ src/app/(admin)/dashboard/newCompanies/page.tsx
@@ -8,7 +8,8 @@ export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main><h1 className="text-xl">Dashboard page</h1>
+    <main><h1 className="text-xl">Dashboard / New Companies</h1>
+    <div>Some additional info</div>
     </main >
   )
 }
